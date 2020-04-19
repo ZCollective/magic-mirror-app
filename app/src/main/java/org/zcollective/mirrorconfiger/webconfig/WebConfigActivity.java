3c34fad965dac95388d0eb4d6213b6d443ba2434
Andreas Knipl <andreas.knipl@zcollective.de>
2020-04-19 20:53:15 +0200

    Clean up WebConfigActivity

--- app/src/main/java/org/zcollective/mirrorconfiger/webconfig/WebConfigActivity.java
+++ app/src/main/java/org/zcollective/mirrorconfiger/webconfig/WebConfigActivity.java
@@ -1,8 +1,8 @@
 package org.zcollective.mirrorconfiger.webconfig;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -25,7 +25,7 @@ public class WebConfigActivity extends AppCompatActivity {
     private FrameLayout webview;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_config);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -35,7 +35,6 @@ public class WebConfigActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-//        configView = findViewById(R.id.webview);
         webview = findViewById(R.id.webview);
 
         SwipeRefreshLayout refreshLayout = findViewById(R.id.swipeContainer);
@@ -43,43 +42,8 @@ public class WebConfigActivity extends AppCompatActivity {
         configView = new WebView(this);
         refreshLayout.setOnRefreshListener(configView::reload);
         configView.setWebChromeClient(new WebChromeClient());
-        configView.setWebViewClient(new WebViewClient() {
-
-            private int running = 0; // Could be public if you want a timer to check.
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView webView, String urlNewString) {
-                running++;
-                webView.loadUrl(urlNewString);
-                return true;
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                running = Math.max(running, 1); // First request move it to 1.
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if(--running == 0) { // just "running--;" if you add a timer.
-                    // TODO: finished... if you want to fire a method.
-                    refreshLayout.setRefreshing(false);
-                }
-            }
-
-
-//            @Override
-//            public void onPageFinished(WebView view, String url) {
-//                // do your stuff here
-//                refreshLayout.setRefreshing(false);
-//            }
-        });
-//        configView.clearCache(true);
+        configView.setWebViewClient(new WebViewClient());
         configureWebView();
-//        configView.getSettings().setJavaScriptEnabled(true);
-//        configView.loadUrl("http://192.168.12.1:8080/");
-
-//        configView.loadUrl(BuildConfig.MIRROR_SETUP_PAGE);
         configView.loadUrl(getIntent().getStringExtra(EXTRA_WEBPAGE));
         webview.addView(configView);
     }
@@ -99,7 +63,6 @@ public class WebConfigActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         configView.stopLoading();
-//        configView.destroy();
         finish();
         onBackPressed();
         return true;
@@ -107,27 +70,29 @@ public class WebConfigActivity extends AppCompatActivity {
 
     @Override
     public void onResume() {
-        super.onResume();
         if (configView != null) configView.resumeTimers();
+        super.onResume();
     }
 
     @Override
     public void onPause() {
-        super.onPause();
         if (configView != null) configView.pauseTimers();
+        super.onPause();
     }
 
     @Override
     protected void onDestroy() {
-//        if (configView != null) configView.destroy();
-//        configView = null;
-        super.onDestroy();
-        configView.clearHistory();
-        configView.clearCache(true);
-        configView.clearSslPreferences();
+        if (configView != null) {
+            configView.stopLoading();
+            configView.clearHistory();
+            configView.clearCache(true);
+            configView.clearSslPreferences();
+            configView.destroy();
+            configView = null;
+        }
+
         webview.removeAllViews();
-        configView.destroy();
-        configView = null;
         webview = null;
+        super.onDestroy();
     }
 }
